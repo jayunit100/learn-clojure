acf84155749d790b8b79ff18c2e92e74df76dfe0
Jay Vyas Peerindex Dev Setup <jayunit100@gmail.com>
2012-04-01 19:26:03 +0100

    added simple unit test for confirmation of char

--- test/problems/test/core.clj
+++ test/problems/test/core.clj
@@ -1,6 +1,9 @@
 (ns problems.test.core
   (:use [problems.core])
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:use [problems.strings]))
 
-(deftest replace-me 
-  (is false "No tests have been written."))
+;;Confirm that the first duplicate is a
+(deftest test-first-duplicate-char
+  (print (first-duplicate-char "abcaD"))
+  (is (= \a (first-duplicate-char "abcaD"))))
