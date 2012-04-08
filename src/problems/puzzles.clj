4ed5e271093ce3ddab69eda934c73178b710420f
jayunit100 <jayunit100@gmail.com>
2012-04-08 09:27:10 -0500

    rectangle points map

--- src/problems/puzzles.clj
+++ src/problems/puzzles.clj
@@ -1,35 +1,54 @@
 (ns problems.puzzles
-  (:require [clojure.string :as cs]))
+  (:require [clojure.string :as cs]) 
+   )
+  
 
 ;;Calculate the max area under a histogram if we 
 ;;have to draw a rectangle which is bounded by all the 
 ;;points.
 
-; A function wrapper that logs the call.  
-(defmacro log [fn-name args & body]
-        `(defn ~fn-name ~args
-           (println "call func -> ..." ~fn-name ~args )
+; A function wrapper that logs the call, and also 
+; runs the pre assertions 
+(defmacro log [fn-name args pre & body]
+        `(defn ~fn-name ~args ~pre
+           (println "call func -> ..." ~fn-name ~args)
            ~@body))
+
+;The seqable method seems to have moved from core. heres a workaround.    
+(defn seqable?
+  [x]
+  (instance? clojure.lang.Seqable x))
 
 ;Returns the number of consecutive elements which 
 ;are >= the compare input.
-(log num-ge [compare hist]
+(log num-ge [hist compare] 
+    {:pre [(number? compare)] }
     (if (= hist '())
       0
       (if (<= compare (first hist))
-          (inc (num-ge compare (rest hist))) 
+          (inc (num-ge (rest hist) compare)) 
            0)))
-
 
 ;Higher performance num-ge ... this time, 
 ;we use tail recursion so the stack is stable.
-(log num-geP [compare hist]
+(log num-geP [hist compare]
+    {:pre [(seqable? hist) (number? compare)]}
     (loop [c compare h hist curr 0]
-     ; (print c " " h " " curr "\n")
+      ; (print c " " h " " curr "\n")
       (if (= h '())
        curr
        (if (not (<= compare (first h)))
            curr
            (recur compare (rest h) (inc curr) )))))
 
-
+;input [2,3,*3*,10,4,12,5,18,6,1], 2
+;returns [3 10, 4 10, 5 10]
+(defn rect-points [histogram i]
+  (let [
+        hist (seq histogram)
+        steps  (num-geP hist (nth hist i))
+        Xs (range i (+ i steps)) 
+        Ys (map #(nth hist %) Xs)
+        r  (zipmap Xs Ys)
+        ]r)
+        )
--- test/problems/test/core.clj
+++ test/problems/test/core.clj
@@ -4,23 +4,34 @@
   (:use [problems.strings])
   (:use [problems.puzzles]))
 
-;;Confirm that the first duplicate is a
+
+  ;;Confirm that the first duplicate is a
 (deftest test-first-duplicate-char
   ;(print (first-duplicate-char "abcaD"))
   (is (= \a (first-duplicate-char "abcaD"))))
 
 ;;Benchmark runs the function "funcge".
 (defn benchmark-ge [funcge]
-  (funcge 2 '(2 3 4 11 1)))
+  (funcge [2 3 4 11 1] 2))
 
 ;;FP example : Benchmark the num-ge function.
 (deftest test-numge
-  (is (= 4 (benchmark-ge num-ge ))))
-
-;;FP example : Benchmark the num-ge (performant) function.
+  (is (= (benchmark-ge num-ge) 4)))
 (deftest test-numgeP
-  (is (= 4 (benchmark-ge num-geP ))))
+  (is (= (benchmark-ge num-geP) 4)))
 
 ;;Now, make sure the performant num-ge works on large sets.
 (deftest test-numgeP_1000
-  (is (= 998 (num-geP 0 (range 2 1000) ))))
+  (is (= 998 (num-geP (range 2 1000) 0))))
+
+;;This helper method can catch arg-swaps that lead to cryptic exceptions.
+;;So its important to test that it works in both + and - situations.
+(deftest test-seqable
+  (is 
+    (and 
+      (= true (seqable? [1 2])) 
+      (= true (seqable? '(1 2))) 
+      (= false (seqable? 3))
+      )
+    )
+  )
